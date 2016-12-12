c0c23999919a890ec64d0d9aecdeb0a5dbaa68b0
unscript <alexkh34@gmail.com>
2016-12-12 21:53:22 +0300

    basic static files

--- app.js
+++ app.js
@@ -1,36 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
 const PORT = 8080;
 
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.get('/', function (req, res) {
-    let msg = 'Got a GET request';
-    res.send(msg);
-    doLog(msg);
-});
-
-app.post("/", (req, res) => {
-    let msg = 'Got a POST request';
-    res.send(msg);
-    doLog(msg);
-});
-
-app.put("/", (req, res) => {
-    let msg = 'Got a PUT request';
-    res.send(msg);
-    doLog(msg);
-});
-
-app.delete("/", (req, res) => {
-    let msg = 'Got a DELETE request';
-    res.send(msg);
-    doLog(msg);
+  res.send('type url static/index.html');
 });
 
 app.listen(PORT, function () {
   console.log('Example app listening on port %s', PORT);
 });
-
-function doLog(msg) {
-    console.log(msg);
-}
